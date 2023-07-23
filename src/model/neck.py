63e67d0ad1f85942cfbbe23b82879ead7b71177a
MarcelloCeresini <mceresini.97@gmail.com>
2023-07-23 19:30:31 +0200

    improved readability on Neck

--- src/model/neck.py
+++ src/model/neck.py
@@ -4,37 +4,45 @@ from torch.nn import ConvTranspose2d, Conv2d
 
 from .deformable_conv_2d import DeformableConv2d
 
-class Neck(Module):
 
-    def __init__(self, init_channels: int) -> None:
+class Upsample(Module):
+    def __init__(self,
+                 in_channels) -> None:
+        
         super().__init__()
-        out_channels_block_1 = int(init_channels/2)
-        self.def_conv1 = DeformableConv2d(init_channels, out_channels_block_1)
-        self.trans_conv1 = ConvTranspose2d(out_channels_block_1, out_channels_block_1, kernel_size=3, stride=2, padding=1, output_padding=1)
 
-        out_channels_block_2 = int(init_channels/4)
-        self.def_conv2 = DeformableConv2d(out_channels_block_1, out_channels_block_2)
-        self.trans_conv2 = ConvTranspose2d(out_channels_block_2, out_channels_block_2, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.out_channels = int(in_channels/2)
 
-        out_channels_block_3 = int(init_channels/8)
-        self.def_conv3 = DeformableConv2d(out_channels_block_2, out_channels_block_3)
-        self.trans_conv3 = ConvTranspose2d(out_channels_block_3, out_channels_block_3, kernel_size=3, stride=2, padding=1, output_padding=1)
-
+        self.def_conv1 = DeformableConv2d(in_channels, 
+                                          self.out_channels)
+        
+        self.trans_conv1 = ConvTranspose2d(self.out_channels, 
+                                           self.out_channels, 
+                                           kernel_size=3, 
+                                           stride=2, 
+                                           padding=1, 
+                                           output_padding=1)
 
     def forward(self, x):
-        # print("feature map shape", x.shape)
-        # y = self.offset_conv_1(x)
-        # print("offsets shape", y.shape)
-        # x = self.def_conv1(x, y)
-        # print("after deformable conv", x.shape)
-        # x = self.trans_conv1(x)
-        # print("after transposed conv", x.shape)
         x = self.def_conv1(x)
         x = self.trans_conv1(x)
-        x = self.def_conv2(x)
-        x = self.trans_conv2(x)
-        x = self.def_conv3(x)
-        x = self.trans_conv3(x)
+        return x
+    
+
+class Neck(Module):
+    def __init__(self,
+                 in_channels: int) -> None:
+        
+        super().__init__()
+
+        self.upsample1 = Upsample(in_channels)
+        self.upsample2 = Upsample(int(in_channels/2))
+        self.upsample3 = Upsample(int(in_channels/4))
+
+    def forward(self, x):
+        x = self.upsample1(x)
+        x = self.upsample2(x)
+        x = self.upsample3(x)
         return x
     
 
