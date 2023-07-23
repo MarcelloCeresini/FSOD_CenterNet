4223e89971c8dfc71c49db4cc26585ad758cc863
MarcelloCeresini <mceresini.97@gmail.com>
2023-07-23 15:35:10 +0200

    corrected return type

--- src/model/backbone.py
+++ src/model/backbone.py
@@ -1,8 +1,8 @@
-import torchvision.models.resnet as Resnets
+from torchvision.models.resnet import ResNet, resnet18
 
 class Backbone:
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str) -> ResNet:
         if name == "resnet18":
-            return Resnets.resnet18(weights='IMAGENET1K_V1', progress = True) 
+            return resnet18(weights='IMAGENET1K_V1', progress = True) 
         else:
             raise NotImplementedError("{} is not an implemented backbone yet".format(name))
