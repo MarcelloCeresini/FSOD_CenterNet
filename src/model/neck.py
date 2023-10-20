d9a3cf3175e444c0ec3d062bd0ea3f984b02b107
Federico Cichetti <volpepe.prog@gmail.com>
2023-10-20 18:00:38 +0200

    Moved neck activation in upsample

--- src/model/neck.py
+++ src/model/neck.py
@@ -1,6 +1,5 @@
 from torch.nn import Module
-from torchvision.ops import DeformConv2d
-from torch.nn import ConvTranspose2d, Conv2d, ReLU
+from torch.nn import ConvTranspose2d, ReLU
 
 from .deformable_conv_2d import DeformableConv2d
 
@@ -13,48 +12,38 @@ class Upsample(Module):
 
         self.out_channels = int(in_channels/2)
 
-        self.def_conv1 = DeformableConv2d(in_channels, 
+        self.def_conv = DeformableConv2d(in_channels, 
                                           self.out_channels)
         
-        self.trans_conv1 = ConvTranspose2d(self.out_channels, 
+        self.trans_conv = ConvTranspose2d(self.out_channels, 
                                            self.out_channels, 
                                            kernel_size=3, 
                                            stride=2, 
                                            padding=1, 
                                            output_padding=1)
+        self.activation = ReLU()
 
     def forward(self, x):
-        x = self.def_conv1(x)
-        x = self.trans_conv1(x)
+        x = self.def_conv(x)
+        x = self.trans_conv(x)
+        x = self.activation(x)
         return x
-    
+
 
 class Neck(Module):
     def __init__(self,
                  in_channels: int) -> None:
         
         super().__init__()
-
         self.upsample1 = Upsample(in_channels)
         self.upsample2 = Upsample(int(in_channels/2))
         self.upsample3 = Upsample(int(in_channels/4))
-
-        self.activation1 = ReLU()
-        self.activation2 = ReLU()
-        self.activation3 = ReLU()
-
         self.out_channels = self.upsample3.out_channels
 
     def forward(self, x):
         x = self.upsample1(x)
-        x = self.activation1(x)
-
         x = self.upsample2(x)
-        x = self.activation2(x)
-
         x = self.upsample3(x)
-        x = self.activation3(x)
-        
         return x
     
 
