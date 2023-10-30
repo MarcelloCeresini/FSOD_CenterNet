f42a3ef6039336fdb615ef9de28832cdc2a98c9d
Federico Cichetti <volpepe.prog@gmail.com>
2023-10-30 18:44:38 +0100

    Added more encoders

--- src/model/encoder.py
+++ src/model/encoder.py
@@ -1,5 +1,5 @@
 from torch.nn import Module
-from torchvision.models.resnet import resnet18
+from torchvision.models.resnet import resnet18, resnet34, resnet50
 from torchvision.models.feature_extraction import create_feature_extractor
 
 class Encoder(Module):
@@ -9,8 +9,13 @@ class Encoder(Module):
 
         if name == "resnet18":
             model = resnet18(weights='IMAGENET1K_V1', progress = True)
-            return_nodes = { "layer4.1.relu_1": "encoder" } # last layer before the classification head
-
+            return_nodes = { "layer4.1.bn2": "encoder" }
+        elif name == 'resnet34':
+            model = resnet34(weights='IMAGENET1K_V1', progress = True)
+            return_nodes = { 'layer4.2.bn2': 'encoder' }
+        elif name == 'resnet50':
+            model = resnet50(weights='IMAGENET1K_V2', progress = True)
+            return_nodes = { 'layer4.2.relu': 'encoder' }
         else:
             raise NotImplementedError("'{}' is not an implemented backbone yet".format(name))
 
